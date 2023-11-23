448c4e7c30ad2a353706a32e9d0ac10669cc5461
colin whittaker <colin@netech.ie>
2023-11-23 15:37:13 +0000

    setting up

---
+++ skell.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.10
+import sys
+
+if __name__ == '__main__':
+    lines = list(line.rstrip() for line in sys.stdin)
+    #do stuff
+    for line in lines:
+        print(line)

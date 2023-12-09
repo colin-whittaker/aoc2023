552fa4bba0cf7b33b1d1fb906dba383f48476e5e
colin whittaker <colin@netech.ie>
2023-12-09 08:12:37 +0000

    day9 OASIS

---
+++ d09.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.10
+import sys
+
+def solve(d,r=False):
+    n = [ x[1]-x[0] for x in zip(d[::],d[1::]) ]
+    if len(set(n)) == 1 and n[0] == 0:
+        return 0
+    n2 = solve(n,r)
+    return n[-1] + n2 if not r else n[0] - n2
+
+if __name__ == '__main__':
+    lines = list(line.rstrip() for line in sys.stdin)
+    #do stuff
+    tot = 0 
+    res = 0
+    for line in lines:
+        data = [ int(x) for x in line.split() ]
+        tot += data[-1] + solve(data)
+        res += data[0] - solve(data,True)
+    print(tot)
+    print(res)

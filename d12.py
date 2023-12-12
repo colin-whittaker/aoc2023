acc17e3166e6f9d6b53c2f65cfae3039c062e370
colin whittaker <colin@netech.ie>
2023-12-12 22:29:16 +0000

    day12 small fixes

--- d12.py
+++ d12.py
@@ -6,7 +6,6 @@ import functools
 def check_strings(s,k):
     res = 0
     key = k
-    i = 0 
     l = len(s)
     ln = k[-1]
     if sum(k) > l:
@@ -22,7 +21,7 @@ def check_strings(s,k):
     for pos in range(l-ln,0,-1):
         if '#' in s[pos+ln:]:
             break
-        if all( c in '#?' for c in s[pos:pos+ln]):
+        if '.' not in s[pos:pos+ln]:
             if s[pos-1] == '#':
                 continue
             res += check_strings(s[:pos-1],k[:max(0,len(k)-1)])
@@ -35,13 +34,13 @@ if __name__ == '__main__':
     for line in lines:
         s,k = line.split()
         data.append((s,tuple([ int(i) for i in k.split(',') ])))
-    for z in [1]:
+    for z in [1,5]:
         tot = 0
         for s,k in data:
             k = k * z
             s2 = s
             for _ in range(z-1):
-                s2 += '?'+s
-            x = check_strings(s2,k)
+                s += '?'+s2
+            x = check_strings(s,k)
             tot += x
         print(tot) 

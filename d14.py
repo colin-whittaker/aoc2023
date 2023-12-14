12074affb4092913c0eed62e2877ef1e0b947452
colin whittaker <colin@netech.ie>
2023-12-14 10:32:42 +0000

    day14 parabolics tidy up

--- d14.py
+++ d14.py
@@ -1,92 +1,50 @@
 #!/usr/bin/env python3.10
 import sys
 
+def tilt_grid(grid,dirs=((-1,0),(0,-1),(1,0),(0,1))):
+    for i,j in dirs:
+        yr,yl,yu = list(range(len(grid))),-1,len(grid)
+        xr,xl,xu = list(range(len(grid[0]))),-1,len(grid[0])
+        if i == -1:
+            yr,yl,yu = list(range(1,len(grid))),0,len(grid)
+        elif i == 1:
+            yr,yl,yu = list(range(len(grid)-1,-1,-1)),-1,len(grid)-1
+        if j == -1:
+            xr,xl,xu = list(range(1,len(grid[0]))),0,len(grid[0])
+        elif j == 1:
+            xr,xl,xu = list(range(len(grid[0])-1,-1,-1)),-1,len(grid[0])-1
+        for y in yr:
+            for x in xr:
+                if grid[y][x] == 'O':
+                    yn,xn=y,x
+                    done = False
+                    while not done:
+                        if yl<yn<yu and xl<xn<xu and grid[yn+i][xn+j] == '.':
+                            yn,xn = yn+i,xn+j
+                        else:
+                            done = True
+                    if yn != y or xn != x:
+                        grid[yn][xn] = grid[y][x]
+                        grid[y][x] = '.'
+    return grid
+
 if __name__ == '__main__':
     lines = list(line.rstrip() for line in sys.stdin)
     #do stuff
     grid = []
     for line in lines:
         grid.append(list(line))
+    grid2 = tilt_grid(grid,[(-1,0)])
+    print(sum([r.count('O')*(len(grid)-i) for i,r in enumerate(grid2)]))
 
-    for y in range(1,len(grid)):
-        for x,c in enumerate(grid[y]):
-            if c == 'O':
-                yn=y
-                done = False
-                while not done:
-                    if yn>0 and grid[yn-1][x] == '.':
-                        yn -= 1
-                    else:
-                        done = True
-                if yn != y:
-                    grid[yn][x] = c
-                    grid[y][x] = '.'
-    print(sum([r.count('O')*(len(grid)-i) for i,r in enumerate(grid)]))
-
-    grid = []
-    for line in lines:
-        grid.append(list(line))
     cache ={}
     values={}
     for z in range(1000000000):
-        for y in range(1,len(grid)):
-            for x,c in enumerate(grid[y]):
-                if c == 'O':
-                    yn=y
-                    done = False
-                    while not done:
-                        if yn>0 and grid[yn-1][x] == '.':
-                            yn -= 1
-                        else:
-                            done = True
-                    if yn != y:
-                        grid[yn][x] = c
-                        grid[y][x] = '.'
-        for y in range(len(grid)):
-            for x in range(1,len(grid[y])):
-                c = grid[y][x]
-                if c == 'O':
-                    xn=x
-                    done = False
-                    while not done:
-                        if xn>0 and grid[y][xn-1] == '.':
-                            xn -= 1
-                        else:
-                            done = True
-                    if xn != x:
-                        grid[y][xn] = c
-                        grid[y][x] = '.'
-        for y in range(len(grid)-1,-1,-1):
-            for x,c in enumerate(grid[y]):
-                if c == 'O':
-                    yn=y
-                    done = False
-                    while not done:
-                        if yn<len(grid)-1 and grid[yn+1][x] == '.':
-                            yn += 1
-                        else:
-                            done = True
-                    if yn != y:
-                        grid[yn][x] = c
-                        grid[y][x] = '.'
-        for y in range(len(grid)):
-            for x in range(len(grid[y])-1,-1,-1):
-                c = grid[y][x]
-                if c == 'O':
-                    xn=x
-                    done = False
-                    while not done:
-                        if xn<len(grid[y])-1 and grid[y][xn+1] == '.':
-                            xn += 1
-                        else:
-                            done = True
-                    if xn != x:
-                        grid[y][xn] = c
-                        grid[y][x] = '.'
+        grid = tilt_grid(grid)
         key = '_'.join([''.join(r) for r in grid])
         if key in cache:
             print(values[cache[key] + (1000000000-1 - cache[key])% (z-cache[key])])
-            break
+            sys.exit()
         else:
             cache[key] = z
-            values[z] = sum([r.count('O')*(len(grid)-i) for i,r in enumerate(grid)])
+            values[z] = sum([r.count('O')*(len(grid)-a) for a,r in enumerate(grid)])

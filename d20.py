7b5293230a86da6641ca8e87ef057d6ef925c3ee
colin whittaker <colin@netech.ie>
2023-12-20 11:24:32 +0000

    day20 propagating pulses -- with code cleanup

--- d20.py
+++ d20.py
@@ -10,14 +10,13 @@ def process_pulse(send,recv,v):
         case 'b':
             for o in nodes[recv]['o']:
                 pulses.append((recv,o,0))
-        case 'f':
-            if v == 0:
-                s = nodes[recv]['s']
-                s = (s+1)%2
+        case '%':
+            if not v:
+                s = (nodes[recv]['s']+1)%2
                 nodes[recv]['s'] = s
                 for o in nodes[recv]['o']:
                     pulses.append((recv,o,s))
-        case 'c':
+        case '&':
             nodes[recv]['i'][send] = v
             v = 1 if 0 in nodes[recv]['i'].values() else 0
             for o in nodes[recv]['o']:
@@ -29,55 +28,41 @@ if __name__ == '__main__':
     nodes = {}
     for line in lines:
         send,recv = line.split(' -> ')
-        recv = recv.split(', ')
-        if '%' == send[0]:
-            t = 'f'
-            send = send[1:]
-        elif '&' == send[0]:
-            t = 'c'
+        if send[0] in '%&':
+            t = send[0]
             send = send[1:]
         else:
             t = 'b'
         nodes[send] = {}
         nodes[send]['t'] = t
-        nodes[send]['o'] = recv
-        if t  == 'c':
+        nodes[send]['o'] = recv.split(', ')
+        if t == '&':
             nodes[send]['i'] = {}
-        if t == 'f':
+        if t == '%':
             nodes[send]['s'] = 0
     for node in nodes:
         for o in nodes[node]['o']:
-            if o in nodes and nodes[o]['t'] == 'c':
+            if o in nodes and nodes[o]['t'] == '&':
                 nodes[o]['i'][node] = 0
-            if o == 'rx':
+            elif o == 'rx':
                 special = node
 
-    nodes2 = copy.deepcopy(nodes)
-    low,high = 0,0
-    for _ in range(1000):
+    inputs = copy.deepcopy(nodes[special]['i'])
+    count,low,high = 0,0,0
+    while True:
         pulses = [('button','broadcaster',0)]
+        count += 1
         while pulses:
             send,recv,v = pulses.pop(0)
             if v:
                 high +=1
             else:
                 low  +=1
-            process_pulse(send,recv,v)
-                       
-    print(low*high)
-
-    inputs = copy.deepcopy(nodes[special]['i'])
-    count = 0
-    rx = False
-    nodes = nodes2
-    while not rx:
-        pulses = [('button','broadcaster',0)]
-        count += 1
-        while pulses:
-            send,recv,v = pulses.pop(0)
             if recv == special and v == 1 and inputs[send] == 0:
                 inputs[send] = count
-            if 0 not in inputs.values():
-                rx = True
             process_pulse(send,recv,v)
+        if count == 1000:
+            print(low*high)
+        if 0 not in inputs.values():
+            break
     print(math.lcm(*inputs.values()) )

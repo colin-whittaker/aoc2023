86a41fe3efb1abf8f673faee192cc0212406efa0
colin whittaker <colin@netech.ie>
2023-12-25 07:47:01 +0000

    day25 - Snowverload

---
+++ d25.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.10
+import sys
+import networkx as nx
+import itertools
+
+if __name__ == '__main__':
+    lines = list(line.rstrip() for line in sys.stdin)
+    #do stuff
+    g = nx.Graph()
+    for line in lines:
+        nodes = line.replace(':', ' ').split()
+        for dest in nodes[1:]:
+            g.add_edge(nodes[0],dest,capacity=1)
+    for a,b in itertools.permutations(g.nodes(),2):
+        cut_size, partition = nx.minimum_cut(g,a,b)
+        if cut_size == 3:
+            print(len(partition[0])*len(partition[1]))
+            break

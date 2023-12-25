e195b24f26e523d4c1d349d55a3423b98610af78
colin whittaker <colin@netech.ie>
2023-12-25 08:04:28 +0000

    day25 - Snowverload

--- d25.py
+++ d25.py
@@ -5,13 +5,12 @@ import itertools
 
 if __name__ == '__main__':
     lines = list(line.rstrip() for line in sys.stdin)
-    #do stuff
     g = nx.Graph()
     for line in lines:
         nodes = line.replace(':', ' ').split()
         for dest in nodes[1:]:
             g.add_edge(nodes[0],dest,capacity=1)
-    for a,b in itertools.permutations(g.nodes(),2):
+    for a,b in itertools.combinations(g.nodes(),2):
         cut_size, partition = nx.minimum_cut(g,a,b)
         if cut_size == 3:
             print(len(partition[0])*len(partition[1]))
